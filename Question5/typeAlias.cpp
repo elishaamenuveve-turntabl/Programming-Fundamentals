81fd59beed65a1a29ad3b34921b5bf9bbe4fcfc2
Elisha Amenuveve <elisha.amenuveve@turntabl.io>
2023-03-21 16:25:27 +0000

    Adding Solution to Question5

---
+++ Question5/typeAlias.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+int main() {
+/* Using the C++ Standard Library string, 
+create a type (using aliasing) for an array of 10 strings. 
+Using the type, create an array and then read 10 string objects from the standard input, 
+and output the string with the largest size.
+ */
+
+    // type aliasing is more readable 
+    using tenStringObjects = array<string, 10> ; 
+    tenStringObjects inputs;
+    int indexOfStringWithLargestSize = 0;
+
+    cout << "Enter 10 words: ";
+    for (int i = 0; i < 10; i++) {
+        cin>> inputs[i];
+        if (inputs[i].size() > inputs[indexOfStringWithLargestSize].size())
+        indexOfStringWithLargestSize = i;
+    }
+
+    cout << "The (first occuring) string input with the largest size is " << inputs[indexOfStringWithLargestSize] << endl;
+
+    return 0;
+}
